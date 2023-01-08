68c992c7ac887b4f00f1a63aee5454dfa2f3a0e8
nessime7 <sara.przebinda@gmail.com>
2023-01-08 14:37:51 +0100

    Commit 2

--- src/main/java/XmlParser.java
+++ src/main/java/XmlParser.java
@@ -10,6 +10,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
 
 public class XmlParser {
+    // proszę o sprawdzenie
     public static void main(String[] args) {
         String file = args[0];
         parser(file);
