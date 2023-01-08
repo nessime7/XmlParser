50f0e51db90ec43af4047736988e804224e484ca
nessime7 <sara.przebinda@gmail.com>
2023-01-08 14:52:54 +0100

    clean up - branch

---
+++ src/test/java/XmlParserTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class XmlParserTest {
+
+    @Test
+    void should_should_return_users_from_users_xml() {
+        // given
+        String file = "users.xml";
+
+        // when
+        var result = XmlParser.parser(file);
+
+        // then
+        assertEquals(result,"name: Kalle Anka email: donald@email.dt userName: donaldd\n" +
+                "name: Joakim von Anka email: scrooge@email.dt userName: onkelscrooge\n" +
+                "name: Arne Anka email: arne@email.com userName: arneanka");
+    }
+}
